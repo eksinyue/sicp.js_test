cf534e08f8d50cb124310aabff51fb6bce1f1fee
eksinyue <xinyuehehxy@gmail.com>
2020-02-13 23:45:50 +0800

    update processingEpigraph func

--- nodejs/processingFunctions/processEpigraph.js
+++ nodejs/processingFunctions/processEpigraph.js
@@ -1,7 +1,7 @@
 import { recursiveProcessText, processText } from '../parseXML';
 
 export const processEpigraph = (node, writeTo) => {
-  writeTo.push("\\epigraph{");
+  writeTo.push("<blockquote class='blockquote'>");
   let child = node.firstChild;
   let attribution = null;
   for (child; child; child = child.nextSibling) {
@@ -14,34 +14,36 @@ export const processEpigraph = (node, writeTo) => {
   }
 
   if (attribution) {
-    writeTo.push("}{--- ");
+    writeTo.push("<div class='chapter-text-ATTRIBUTION'>\n");
 
     const author = attribution.getElementsByTagName("AUTHOR")[0];
     if (author) {
-      writeTo.push("\\textup{");
+      writeTo.push("<span class='chapter-text-AUTHOR'>");
       recursiveProcessText(author.firstChild, writeTo);
-      writeTo.push("}");
+      writeTo.push("</span>\n");
     }
 
     child = attribution.getElementsByTagName("TITLE")[0];
     if (child) {
-      if (author) writeTo.push(", ");
+      if (author) writeTo.push("<span class='chapter-text-TITLE'>");
       recursiveProcessText(child.firstChild, writeTo);
+      writeTo.push("</span>\n");
     }
 
     child = attribution.getElementsByTagName("DATE")[0];
     if (child) {
-      writeTo.push(" (");
+      writeTo.push("<span class='chapter-text-DATE'>");
       recursiveProcessText(child.firstChild, writeTo);
-      writeTo.push(")");
+      writeTo.push("</span>\n");
     }
 
     child = attribution.getElementsByTagName("INDEX")[0];
     if (child) {
       processText(child, writeTo);
     }
+    writeTo.push("</div>\n");
   }
-  writeTo.push("}\n");
+  writeTo.push("</blockquote>\n");
 };
 
 export default processEpigraph;
